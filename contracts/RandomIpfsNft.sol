4527ec6c68577dff387a58e166d43f24eafcff63
AnoopVL <avlanjekar4@gmail.com>
2023-03-29 23:22:52 +0530

    RandomIpfsNft.sol created

---
+++ contracts/RandomIpfsNft.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract RandomIpfsNft {
+  function requestNft() public {}
+
+  function fullfilRandomWords(
+    uint256 requestId,
+    uint256[] memory randomWords
+  ) internal {}
+
+  function tokenURI(uint256) public {}
+}
