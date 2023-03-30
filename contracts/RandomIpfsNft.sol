9ee0d4ecc9bbb025838a804fa51e0fe20b820e65
AnoopVL <avlanjekar4@gmail.com>
2023-03-30 23:01:31 +0530

    RandomIpfsNft.sol added

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -2,13 +2,44 @@
 
 pragma solidity ^0.8.7;
 
-contract RandomIpfsNft {
-  function requestNft() public {}
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract RandomIpfsNft is VRFConsumerBaseV2 {
+  VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+  uint64 private immutable i_subscriptionId;
+  bytes32 private immutable i_gasLane;
+  uint32 private immutable i_callbackGasLimit;
+  uint16 private constant REQUEST_CONFIRMATIONS = 3;
+  uint32 private constant NUM_WORDS = 1;
+
+  constructor(
+    address vrfCoordinatorV2,
+    uint64 i_subscriptionId,
+    bytes32 i_gasLane,
+    uint32 i_callbackGasLimit,
+    uint16 REQUEST_CONFIRMATIONS,
+    uint32 NUM_WORDS
+  ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+    i_subscriptionId = subscriptionId;
+    i_gasLane = gasLane;
+    i_callbackGasLimit = callbackGasLimit;
+  }
+
+  function requestNft() public returns (uint256 requestId) {
+    requestId = i_vrfCoordinator.requestRandomWords(
+      i_gasLane,
+      i_subscriptionId,
+      REQUEST_CONFIRMATIONS,
+      i_callbackGasLimit
+    );
+  }
 
   function fullfilRandomWords(
     uint256 requestId,
     uint256[] memory randomWords
-  ) internal {}
+  ) internal override {}
 
   function tokenURI(uint256) public {}
 }
